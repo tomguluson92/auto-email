c72218b41526fbaa3674fa7e6ec989f2d7284018
tomguluson92 <314913739@qq.com>
2016-09-12 10:07:44 +0800

    Update gen_bannercategory_report.py

--- gen_bannercategory_report.py
+++ gen_bannercategory_report.py
@@ -7,12 +7,13 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 from mybasic import GenReport
 from application import GetDownloadRedirectData
-
+#GenReport、GetDownloadRedirectData是两个类：其中，GenReport是生成报告(HTML格式)， GetDownloadRedirectData是获取数据源。
 class GenDownloadRedirectReport(GenReport.GenReport):
 
     def __init__(self, opt):
         super(GenDownloadRedirectReport, self).__init__(opt, 'bannercategory')
-
+    
+    #bannercategory.tpl是模板
     def gen_report(self):
         data = GetDownloadRedirectData.DownloadRedirectData(self.date)
         html = self.data2html(data.get_download_redirect_data(), 'bannercategory.tpl')
